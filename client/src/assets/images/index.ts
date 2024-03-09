12ac632577547efcfd36d6704fce4d80ca1e4e33
Suyash K. <suyu162001@gmail.com>
2024-03-09 22:13:42 +0530

    chore: png to webp in assets/index.ts

--- client/src/assets/images/index.ts
+++ client/src/assets/images/index.ts
@@ -1,4 +1,4 @@
-export const icon = new URL("./icon.png", import.meta.url).href
-export const iconwhite = new URL("./iconwhite.png", import.meta.url).href
-export const logo = new URL("./logo.png", import.meta.url).href
-export const logowhite = new URL("./logowhite.png", import.meta.url).href
+export const icon = new URL("./icon.webp", import.meta.url).href
+export const iconwhite = new URL("./iconwhite.webp", import.meta.url).href
+export const logo = new URL("./logo.webp", import.meta.url).href
+export const logowhite = new URL("./logowhite.webp", import.meta.url).href
