1ba5763d26d44167b5befd74155251c8e06aa8d3
Suyash K. <suyu162001@gmail.com>
2024-03-18 12:56:21 +0530

    fix: improvement in meta controller

--- server/src/controllers/meta.ts
+++ server/src/controllers/meta.ts
@@ -3,47 +3,52 @@ import Invite from "../models/invite";
 import Reading from "../models/reading";
 
 export const getMetaData = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const readingCount = await Reading.countDocuments();
-        const invitesCount = await Invite.countDocuments();
-        const result = await Invite.aggregate([
-            {
-              $lookup: {
-                from: 'readings', // assuming 'readings' is the name of your readings collection
-                localField: 'reading',
-                foreignField: '_id',
-                as: 'readingData'
-              }
-            },
-            {
-              $unwind: '$readingData'
-            },
-            {
-              $match: {
-                'readingData.readBy': { $in: ['chapter', 'verse'] }
-              }
-            },
-            {
-              $group: {
-                _id: '$readingData.readBy',
-                count: { $sum: 1 }
-              }
-            }
-          ]);
-      
-          const counts = { ChapterCount: 0, VerseCount: 0, readingCount, invitesCount };
-      
-          result.forEach((item: { _id: string; count: number }) => {
-            if (item._id === 'chapter') {
-              counts.ChapterCount = item.count;
-            } else if (item._id === 'verse') {
-              counts.VerseCount = item.count;
-            }
-          });
-      
-          res.json(counts);
-        
-    } catch (error) {
-        next({ status: 404, message: "Not Found", detail: "No Data found"})
-    }
+  try {
+    const readingCount = await Reading.countDocuments();
+    const invitesCount = (await Invite.find({ status: "read" })).length;
+    const result = await Invite.aggregate([
+      {
+        $match: {
+          status: 'read'
+        }
+      },
+      {
+        $lookup: {
+          from: 'readings',
+          localField: 'reading',
+          foreignField: '_id',
+          as: 'readingData'
+        }
+      },
+      {
+        $unwind: '$readingData'
+      },
+      {
+        $match: {
+          'readingData.readBy': { $in: ['chapter', 'verse'] }
+        }
+      },
+      {
+        $group: {
+          _id: '$readingData.readBy',
+          count: { $sum: 1 }
+        }
+      }
+    ]);
+
+    const counts = { ChapterCount: 0, VerseCount: 0, readingCount, invitesCount };
+
+    result.forEach((item: { _id: string; count: number }) => {
+      if (item._id === 'chapter') {
+        counts.ChapterCount = item.count;
+      } else if (item._id === 'verse') {
+        counts.VerseCount = item.count;
+      }
+    });
+
+    res.json(counts);
+
+  } catch (error) {
+    next({ status: 404, message: "Not Found", detail: "No Data found" })
+  }
 }
